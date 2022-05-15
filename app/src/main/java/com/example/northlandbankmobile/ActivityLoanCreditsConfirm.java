13edb68a2f54b2248b2735f968ab6493042c2602
leonDev06 <leonnel.dimatulac@neu.edu.ph>
2022-05-15 13:34:35 +0800

    Code Optimization  Classes extending Thread which were used to generate PDFs were made as inner-classes of their respecting activity wherein they are called. This allowed to no longer pass the Views as parameters to the constructors as they now can be accessed directly from the activity. Functions  -Added a spinning loading icon which will be shown to the users to notify them that their PDF receipt is currently being generated. This runs on a different thread to allow to periodically check whether the PDF generation has completed or not.

--- app/src/main/java/com/example/northlandbankmobile/ActivityLoanCreditsConfirm.java
+++ app/src/main/java/com/example/northlandbankmobile/ActivityLoanCreditsConfirm.java
@@ -10,9 +10,10 @@ import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.os.Looper;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,6 +25,7 @@ public class ActivityLoanCreditsConfirm extends AppCompatActivity {
     //Initialize Widgets
     private TextView mAmount, mRefNum, mCurrentDate, mDueDate;
     private Button buttonPrintPdf, buttonHome;
+    private ProgressBar progressBar;
 
     int pageHeight = 1120;
     int pageWidth = 792;
@@ -53,20 +55,17 @@ public class ActivityLoanCreditsConfirm extends AppCompatActivity {
         mRefNum = findViewById(R.id.loanSuccessRefNum);
         mCurrentDate = findViewById(R.id.loanSuccessCurrentDate);
         mDueDate = findViewById(R.id.loanSuccessDueDate);
+        progressBar = findViewById(R.id.loanProgressBar);
 
         //Display data
         displayData();
-
-
 
         //Clickable Buttons
         buttonPrintPdf = findViewById(R.id.fragSendMoneyBtnBack);
         buttonPrintPdf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ThreadGeneratePdfLoan generatePdfLoan = new ThreadGeneratePdfLoan
-                        (getApplicationContext(), mAmount, mCurrentDate, mDueDate, mRefNum);
-                generatePdfLoan.start();
+                generatePdfReceipt();
             }
         });
         buttonHome = findViewById(R.id.fragSendMoneyBtnSend);
@@ -103,5 +102,86 @@ public class ActivityLoanCreditsConfirm extends AppCompatActivity {
         mCurrentDate.setText(date);
         mDueDate.setText(dateDue);
         mRefNum.setText(refNum);
+    }
+
+    private void generatePdfReceipt(){
+        //Start the thread that generates the PDF
+        //This thread will keep running until a PDF receipt has been generated
+        ThreadGeneratePdfLoan generatePdfLoan = new ThreadGeneratePdfLoan();
+        generatePdfLoan.start();
+
+        //Start the thread that handles whether to show or not the spinning loading icon
+        //This thread will display the progress icon while the generate PDF thread is running, and will hide it when the PDF thread finishes
+        //This thread will keep running until PDF receipt has been generated
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean runThread = true;
+                while (runThread) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (generatePdfLoan.isAlive()) {
+                                progressBar.setVisibility(View.VISIBLE);
+                            } else {
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                    });
+                    if(!generatePdfLoan.isAlive()){
+                        runThread=false;
+                    }
+                }
+            }
+        }).start();
+    }
+
+
+    //Thread Class for generating a PDF receipt
+    class ThreadGeneratePdfLoan extends Thread {
+
+        public void run() {
+            //Creates a PdfDocument
+            PdfDocument pdfDocument = new PdfDocument();
+
+            //The painter which will be used to paint inside the canvas
+            Paint text = new Paint();
+
+            //Create the PDF page information
+            PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(792, 150, 1).create();
+            PdfDocument.Page myPage = pdfDocument.startPage(myPageInfo);
+
+            //The canvas wherein the information will be drawn. The page of the PDF.
+            Canvas canvas = myPage.getCanvas();
+
+            //Initialize and setup the painter.
+            text.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+            text.setTextSize(15);
+            text.setColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+
+            //Paint the information
+            canvas.drawText("LOAN", 209, 40, text);
+            canvas.drawText(("AMOUNT: " + mAmount.getText().toString()), 209, 60, text);
+            canvas.drawText(("DATE: " + mCurrentDate.getText().toString()), 209, 80, text);
+            canvas.drawText(("DATE DUE: " + mDueDate.getText().toString()), 209, 100, text);
+            canvas.drawText(("REFERENCE NUMBER: " + mRefNum.getText().toString()), 209, 120, text);
+
+            //Finish
+            pdfDocument.finishPage(myPage);
+
+            //Create a PDF file in the phone storage
+            File file = new File(Database.commonDocumentDirPath
+                    ("Northland Bank Receipts"), ("/Loan_" + mRefNum.getText().toString() + "_Receipt.pdf"));
+
+            //Store the generated Pdf info in the PDF file created in the phone storage
+            try {
+                pdfDocument.writeTo(new FileOutputStream(file));
+                Looper.prepare();
+                Toast.makeText(getApplicationContext(), "PDF Receipt Stored in Documents Folder", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            pdfDocument.close();
+        }
     }
 }
--- app/src/main/java/com/example/northlandbankmobile/ActivitySendMoneySuccess.java
+++ app/src/main/java/com/example/northlandbankmobile/ActivitySendMoneySuccess.java
@@ -1,15 +1,21 @@
 package com.example.northlandbankmobile;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +26,7 @@ import java.io.IOException;
 public class ActivitySendMoneySuccess extends AppCompatActivity {
     private Button btnHome, btnPdf;
     private TextView mUserName, mFullName, mAmount, mDate, mRefNum;
+    private ProgressBar progressBar;
 
     private TransactionManager transactionManager;
     private Navigator navigator;
@@ -40,6 +47,7 @@ public class ActivitySendMoneySuccess extends AppCompatActivity {
         mAmount = findViewById(R.id.amountConfirm);
         mDate = findViewById(R.id.actSendSuccessDate);
         mRefNum = findViewById(R.id.actSendSuccesRefNum);
+        progressBar = findViewById(R.id.progressBar);
 
 
         transactionManager = new TransactionManager();
@@ -60,8 +68,7 @@ public class ActivitySendMoneySuccess extends AppCompatActivity {
         btnPdf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ThreadGeneratePdfSendMoney generatePdf = new ThreadGeneratePdfSendMoney(mUserName, mAmount, mDate, mRefNum, getApplicationContext());
-                generatePdf.start();
+                generatePdfReceipt();
             }
         });
     }
@@ -83,8 +90,6 @@ public class ActivitySendMoneySuccess extends AppCompatActivity {
         navigator.setGoingToAnotherActivity(false);
     }
 
-    //Private Methods
-
     //Retrieves data that was sent from FragmentSendMoneyConfirm and use it to display data on the corresponding TextView
     private void displayDataOnUi(){
         mUserName.setText(getIntent().getStringExtra(FragmentSendMoneyConfirm.KEY_USERNAME));
@@ -92,5 +97,71 @@ public class ActivitySendMoneySuccess extends AppCompatActivity {
         mAmount.setText(getIntent().getStringExtra(FragmentSendMoneyConfirm.KEY_AMOUNT_SENT));
         mDate.setText(new DateManager().getCurrentDate().toString());
         mRefNum.setText(getIntent().getStringExtra(FragmentSendMoneyConfirm.KEY_REF_NUM));
+    }
+
+    private void generatePdfReceipt(){
+        ThreadGeneratePdfSendMoney generatePdf = new ThreadGeneratePdfSendMoney();
+        generatePdf.start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean runThread = true;
+                while (runThread) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (generatePdf.isAlive()) {
+                                progressBar.setVisibility(View.VISIBLE);
+                            } else {
+                                progressBar.setVisibility(View.INVISIBLE);
+                            }
+                        }
+                    });
+                    if(!generatePdf.isAlive()){
+                        runThread=false;
+                    }
+                }
+            }
+        }).start();
+    }
+
+    class ThreadGeneratePdfSendMoney extends Thread{
+        public void run(){
+            //See ActivityLoanCreditsConfirm's documentation on how generating a Pdf File works
+            //Has the same flow as ActivityLoanConfirm's thread, but different information
+            PdfDocument pdfDocument = new PdfDocument();
+
+            Paint text = new Paint();
+
+            PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(792, 150, 1).create();
+
+            PdfDocument.Page myPage = pdfDocument.startPage(myPageInfo);
+
+            Canvas canvas = myPage.getCanvas();
+
+            text.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+            text.setTextSize(15);
+            text.setColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+
+            canvas.drawText("SEND MONEY", 209, 40, text);
+            canvas.drawText(("RECEIVER: "+mUserName.getText().toString()), 209, 60, text);
+            canvas.drawText(("AMOUNT: "+mAmount.getText().toString()), 209, 80, text);
+            canvas.drawText(("DATE: "+mDate.getText().toString()), 209, 100, text);
+            canvas.drawText(("REFERENCE NUMBER: "+mRefNum.getText().toString()), 209, 120, text);
+
+            pdfDocument.finishPage(myPage);
+
+            File file = new File(Database.commonDocumentDirPath
+                    ("Northland Bank Receipts"), ("/SendMoney_"+Database.getCurrentUser()+"_"+mRefNum.getText().toString()+"_Receipt.pdf"));
+            try {
+                pdfDocument.writeTo(new FileOutputStream(file));
+                Looper.prepare();
+                Toast.makeText(getApplicationContext(), "PDF Receipt Stored in Documents Folder", Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            pdfDocument.close();
+        }
     }
 }
